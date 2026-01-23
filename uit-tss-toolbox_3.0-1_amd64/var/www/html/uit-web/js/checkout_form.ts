73ce5c45f1f6276e33ee8c8e6fcc6c15445e0e7b
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2026-01-23 12:14:55 -0600

    Update

--- uit-tss-toolbox_3.0-1_amd64/var/www/html/uit-web/js/checkout_form.ts
+++ uit-tss-toolbox_3.0-1_amd64/var/www/html/uit-web/js/checkout_form.ts
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const printCheckoutFormContainer = document.getElementById('print-checkout-form-container') as HTMLElement;
-	const customerName = document.getElementById('print-customer-name') as HTMLInputElement;
-	const customerPSID = document.getElementById('print-customer-psid') as HTMLInputElement;
+	const customerNameEl = document.getElementById('print-customer-name') as HTMLInputElement;
+	const customerPSIDEl = document.getElementById('print-customer-psid') as HTMLInputElement;
 	const checkoutDateEl = document.getElementById('print-checkout-date') as HTMLInputElement;
 	const returnDateEl = document.getElementById('print-return-date') as HTMLInputElement;
 	const confirmAndPrint = document.getElementById('confirm-and-print') as HTMLElement;
@@ -15,10 +15,26 @@ document.addEventListener('DOMContentLoaded', () => {
 	};
 
 	confirmAndPrint.addEventListener('click', () => {
-		(document.querySelector('#customer_name') as HTMLElement).textContent = customerName.value || '';
-		(document.querySelector('#customer_psid') as HTMLElement).textContent = customerPSID.value || '';
-		(document.querySelector('#checkout_date') as HTMLElement).textContent = checkoutDateEl.value ? new Date(checkoutDateEl.value + "T00:00:00").toLocaleDateString('en-US', options) : '';
-		(document.querySelector('#return_date') as HTMLElement).textContent = returnDateEl.value ? new Date(returnDateEl.value + "T00:00:00").toLocaleDateString('en-US', options) : '';
+
+		const customerName = customerNameEl.value.trim() || '';
+		const customerPSID = customerPSIDEl.value.trim() || '';
+		const checkoutDateStr = checkoutDateEl.value.trim() || '';
+		const returnDateStr = returnDateEl.value.trim() || '';
+
+
+		(document.querySelector('#customer_name') as HTMLElement).textContent = customerName;
+		(document.querySelector('#customer_psid') as HTMLElement).textContent = customerPSID;
+		(document.querySelector('#checkout_date') as HTMLElement).textContent =  checkoutDateStr ? new Date(checkoutDateStr + "T00:00:00").toLocaleDateString('en-US', options) : '';
+		(document.querySelector('#return_date') as HTMLElement).textContent = returnDateStr ? new Date(returnDateStr + "T00:00:00").toLocaleDateString('en-US', options) : '';
+
+		if (customerName || customerPSID) {
+			const urlParams = new URLSearchParams(window.location.search)
+			let fileName = urlParams.get("tagnumber") + " Checkout Form";
+			if (customerName) fileName = fileName + ` - ${customerName}`;
+			if (customerPSID) fileName = fileName + ` (${customerPSID})`;
+			window.document.title = fileName;
+		}
+
 		window.print();
 
 		window.addEventListener('afterprint', () => {
