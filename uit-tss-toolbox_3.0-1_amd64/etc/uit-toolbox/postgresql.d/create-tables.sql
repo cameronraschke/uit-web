53fee153d89a0bc4d8f6c8589baa605ebe4560fb
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2026-03-13 10:44:14 -0500

    Update create-tables.sql

--- uit-tss-toolbox_3.0-1_amd64/etc/uit-toolbox/postgresql.d/create-tables.sql
+++ uit-tss-toolbox_3.0-1_amd64/etc/uit-toolbox/postgresql.d/create-tables.sql
@@ -278,8 +278,8 @@ INSERT INTO static_bios_stats
 VALUES
 	('HP ProBook 450 G6', 'R71 Ver. 01.33.00'),
 	('Dell Pro Slim Plus QBS1250', '1.6.2'),
-	('Latitude 7400', '1.41.1'),
-	('OptiPlex 7000', '1.31.1'),
+	('Latitude 7400', '1.43.0'),
+	('OptiPlex 7000', '1.38.0'),
 	('Latitude 7420', '1.43.1'),
 	('Latitude 3500', '1.36.0'),
 	('Latitude 3560', 'A19'),
