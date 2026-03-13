8ada45fcde3179d873d3a035c24ab5e7fbd9764a
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2026-03-13 08:48:06 -0500

    Update select-repo.go

--- uit-tss-toolbox_3.0-1_amd64/opt/uit-toolbox/uit-web/database/select-repo.go
+++ uit-tss-toolbox_3.0-1_amd64/opt/uit-toolbox/uit-web/database/select-repo.go
@@ -625,7 +625,7 @@ func (repo *SelectRepo) GetLocationFormData(ctx context.Context, tag *int64, ser
 	LEFT JOIN checkout_log ON locations.tagnumber = checkout_log.tagnumber AND checkout_log.log_entry_time IN (SELECT MAX(log_entry_time) FROM checkout_log WHERE log_entry_time IS NOT NULL GROUP BY tagnumber)
 	LEFT JOIN static_department_info ON locations.department_name = static_department_info.department_name
 	LEFT JOIN client_images ON locations.tagnumber = client_images.tagnumber
-	INNER JOIN default_system_model
+	CROSS JOIN default_system_model
 	WHERE (locations.tagnumber = $1 OR locations.system_serial = $2)
 	ORDER BY locations.time DESC NULLS LAST
 	LIMIT 1;`
