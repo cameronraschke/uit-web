ad7fe29eeebbb15f7e8902edb66cdda9f5a3907c
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2026-06-08 20:32:39 -0500

    Update logout.ts

--- uit-tss-toolbox_3.0-1_amd64/var/www/html/uit-web/ts/logout.ts
+++ uit-tss-toolbox_3.0-1_amd64/var/www/html/uit-web/ts/logout.ts
@@ -1,36 +1,43 @@
-let isLoggingOut: boolean = false;
+var isLoggingOut: boolean = false;
 
 const logoutAuthChannel = new BroadcastChannel('auth');
 logoutAuthChannel.onmessage = function(event) {
-  console.log(event);
-  if (event.data.cmd === 'logout') {
-    logout();
-  }
+	console.log(event);
+	if (event.data.cmd === 'logout') {
+		logout();
+	}
 };
 
-const logoutButton = document.getElementById("menu-logout-button") as HTMLButtonElement;
-logoutButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  logout();
-});
+const logoutButton = document.getElementById("menu-logout-button") as HTMLButtonElement | null;
+if (logoutButton !== null) {
+	logoutButton.addEventListener("click", function(event) {
+		event.preventDefault();
+		this.blur();
+		this.disabled = true;
+		logout();
+	});
+}
 
 async function logout() {
-  if (isLoggingOut) return;
-  isLoggingOut = true;
-  logoutButton.disabled = true;
-  logoutAuthChannel.postMessage({cmd: 'logout'});
-  localStorage.clear();
-  sessionStorage.clear();
+	if (isLoggingOut) {
+		console.warn("Logout already in progress, ignoring additional logout request.");
+		return;
+	}
+	isLoggingOut = true;
 
-  let logoutUrl = "/logout";
-  const currentPath = window.location.pathname;
-  if (currentPath !== "/login" && currentPath !== "/logout") {
-    logoutUrl += "?redirect=" + encodeURIComponent(currentPath + window.location.search);
-  }
+	logoutAuthChannel.postMessage({cmd: 'logout'});
+	localStorage.clear();
+	sessionStorage.clear();
 
-  window.location.replace(logoutUrl);
+	let logoutUrl = "/logout";
+	// const currentPath = window.location.pathname;
+	// if (currentPath !== "/login" && currentPath !== "/logout") {
+	// 	logoutUrl += "?redirect=" + encodeURIComponent(currentPath + window.location.search);
+	// }
+
+	window.location.replace(logoutUrl);
 }
 
 if (window.location.pathname === '/logout') {
-  logout();
+	logout();
 }
